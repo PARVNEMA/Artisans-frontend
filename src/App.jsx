d0bac4753f545262c3c195c7461916510a92b02a
Harsh Kumar Mahto <harshkumaraunak@gmail.com>
2024-11-07 23:13:35 +0530

    Folder created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h3 className='bg-red-600'>Hello</h3>
+      <p className='p-5 m-2 text-green-700 bg-yellow-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas minus ipsam, doloribus similique officia quam excepturi vel? Architecto, non a amet magnam similique quisquam corporis recusandae, doloribus id iste, quam libero? Quia odit aperiam tempore praesentium, mollitia explicabo quis.</p>
+      <button type="link" variant='filled'>Button</button>
+    </>
+  )
+}
+
+export default App
