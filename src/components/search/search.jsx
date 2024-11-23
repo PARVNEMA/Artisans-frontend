ff2ec1c9e9e39334fa68f58c9e669652bd629c44
RabdeepKaur <ravleen0108@gmail.com>
2024-11-23 16:13:02 +0530

    work in progress

---
+++ src/components/search/search.jsx
@@ -0,0 +1,12 @@
+import React,{useState} from "react";
+import axios from "axios";
+
+const IamgeSearch=()=>{
+
+    
+    const[results,setResults]=useState([]);
+
+    const handleImageUpload=async(file)=>{
+
+    }
+}
