3bc3d81522ff1c098fbb4d489c0f3acfc0f5cd65
parv nema <115030244+PARVNEMA@users.noreply.github.com>
2024-11-25 21:53:59 +0530

    other artisans changes maded

--- src/components/Artisans/dashboard/Dashboard.jsx
+++ src/components/Artisans/dashboard/Dashboard.jsx
@@ -1,247 +1,324 @@
 import axios from "axios";
-import React, { useCallback, useEffect, useState } from "react";
+import React, {
+	useCallback,
+	useEffect,
+	useState,
+} from "react";
 import {
-  PieChart,
-  Pie,
-  Tooltip,
-  Cell,
-  BarChart,
-  Bar,
-  CartesianGrid,
-  XAxis,
-  YAxis,
+	PieChart,
+	Pie,
+	Tooltip,
+	Cell,
+	BarChart,
+	Bar,
+	CartesianGrid,
+	XAxis,
+	YAxis,
 } from "recharts";
 import { useArtisansAuth } from "../../../../useContext/ArtisansContext.jsx";
 import { Link } from "react-router-dom";
 import { IndianRupee, IndianRupeeIcon } from "lucide-react";
 
 function Dashboard() {
-  {
-    const backendurl = import.meta.env.VITE_URL;
-    const [artisansproducts, setArtisansProducts] = useState([]);
-    const [artisansmatrices, setArtisansMatrices] = useState(null);
-    const [artisans, setArtisans] = useState(null);
-    const getArtisansProduct = useCallback(async () => {
-      try {
-        const res = await axios.get(`${backendurl}/products/myProducts`, {
-          withCredentials: true,
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem(
-              "artisansaccessToken"
-            )}`,
-          },
-        });
-        console.log("current artisans products", res.data);
-        setArtisansProducts(res.data.data);
-      } catch (error) {
-        console.error("error in dashboard ", error);
-      }
-    }, []);
-    const getArtisansSellerMetrices = useCallback(async () => {
-      try {
-        const res = await axios.get(
-          `${backendurl}/artisans/matrices/${artisans?._id}`,
-          {
-            withCredentials: true,
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem(
-                "artisansaccessToken"
-              )}`,
-            },
-          }
-        );
-        console.log("current artisans metrices", res.data);
-        setArtisansMatrices(res.data.data);
-      } catch (error) {
-        console.error("error in dashboard ", error);
-      }
-    }, [artisans?._id]);
-    const getCurrentArtisans = useCallback(async () => {
-      try {
-        const res = await axios.get(`${backendurl}/artisans/current-user`, {
-          withCredentials: true,
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem(
-              "artisansaccessToken"
-            )}`,
-          },
-        });
-        console.log("res in getcurrent artisans", res.data);
-        setArtisans(res.data.data);
-      } catch (error) {
-        console.log("Error", error);
-      }
-    }, []);
-    useEffect(() => {
-      getCurrentArtisans();
-    }, [getCurrentArtisans]);
-    useEffect(() => {
-      if (artisans) {
-        console.log("User is logged in, fetching products...");
-        getArtisansProduct();
-        getArtisansSellerMetrices();
-      }
-    }, [artisans, getArtisansProduct, getArtisansSellerMetrices]);
+	{
+		const backendurl = import.meta.env.VITE_URL;
+		const [artisansproducts, setArtisansProducts] =
+			useState([]);
+		const [artisansmatrices, setArtisansMatrices] =
+			useState(null);
+		const [artisans, setArtisans] = useState(null);
+		const getArtisansProduct = useCallback(async () => {
+			try {
+				const res = await axios.get(
+					`${backendurl}/products/myProducts`,
+					{
+						withCredentials: true,
+						headers: {
+							Authorization: `Bearer ${localStorage.getItem(
+								"artisansaccessToken"
+							)}`,
+						},
+					}
+				);
+				console.log("current artisans products", res.data);
+				setArtisansProducts(res.data.data);
+			} catch (error) {
+				console.error("error in dashboard ", error);
+			}
+		}, []);
+		const getArtisansSellerMetrices =
+			useCallback(async () => {
+				try {
+					const res = await axios.get(
+						`${backendurl}/artisans/matrices/${artisans?._id}`,
+						{
+							withCredentials: true,
+							headers: {
+								Authorization: `Bearer ${localStorage.getItem(
+									"artisansaccessToken"
+								)}`,
+							},
+						}
+					);
+					console.log(
+						"current artisans metrices",
+						res.data
+					);
+					setArtisansMatrices(res.data.data);
+				} catch (error) {
+					console.error("error in dashboard ", error);
+				}
+			}, [artisans?._id]);
+		const getCurrentArtisans = useCallback(async () => {
+			try {
+				const res = await axios.get(
+					`${backendurl}/artisans/detail`,
+					{
+						withCredentials: true,
+						headers: {
+							Authorization: `Bearer ${localStorage.getItem(
+								"artisansaccessToken"
+							)}`,
+						},
+					}
+				);
+				console.log(
+					"res in getcurrent artisans details =",
+					res.data
+				);
+				setArtisans(res.data.data.artisan);
+			} catch (error) {
+				console.log("Error", error);
+			}
+		}, []);
+		useEffect(() => {
+			getCurrentArtisans();
+		}, [getCurrentArtisans]);
+		useEffect(() => {
+			if (artisans) {
+				console.log(
+					"User is logged in, fetching products..."
+				);
+				getArtisansProduct();
+				getArtisansSellerMetrices();
+			}
+		}, [
+			artisans,
+			getArtisansProduct,
+			getArtisansSellerMetrices,
+		]);
 
-    //For pie chart
-    const [activeIndex, setActiveIndex] = useState(-1);
+		//For pie chart
+		const [activeIndex, setActiveIndex] = useState(-1);
 
-    const data = [
-      { name: "Returns", students: 400 },
-      { name: "Sales", students: 700 },
-      { name: "Disputes", students: 200 },
-      { name: "Total", students: 1000 },
-    ];
+		const data = [
+			{ name: "Returns", students: 400 },
+			{ name: "Sales", students: 700 },
+			{ name: "Disputes", students: 200 },
+			{ name: "Total", students: 1000 },
+		];
 
-    const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+		const COLORS = [
+			"#0088FE",
+			"#00C49F",
+			"#FFBB28",
+			"#FF8042",
+		];
 
-    const onPieEnter = (_, index) => {
-      setActiveIndex(index);
-    };
+		const onPieEnter = (_, index) => {
+			setActiveIndex(index);
+		};
 
-    // Bar graph
-    const data2 = [
-      { name: "Jan", sales: 400 },
-      { name: "Feb", sales: 700 },
-      { name: "Mar", sales: 200 },
-      { name: "Apr", sales: 1000 },
-      { name: "May", sales: 400 },
-      { name: "Jun", sales: 700 },
-      { name: "Jul", sales: 200 },
-      { name: "Aug", sales: 1000 },
-      { name: "Sep", sales: 400 },
-      { name: "Oct", sales: 700 },
-      { name: "Nov", sales: 200 },
-      { name: "Dec", sales: 1000 },
-    ];
+		// Bar graph
+		const data2 = [
+			{ name: "Jan", sales: 400 },
+			{ name: "Feb", sales: 700 },
+			{ name: "Mar", sales: 200 },
+			{ name: "Apr", sales: 1000 },
+			{ name: "May", sales: 400 },
+			{ name: "Jun", sales: 700 },
+			{ name: "Jul", sales: 200 },
+			{ name: "Aug", sales: 1000 },
+			{ name: "Sep", sales: 400 },
+			{ name: "Oct", sales: 700 },
+			{ name: "Nov", sales: 200 },
+			{ name: "Dec", sales: 1000 },
+		];
 
-    return (
-      <div>
-        <div className="flex items-center w-full">
-          {/* Header */}
-          <div className="w-[85%] pl-[8rem]">
-            <h1 className="text-5xl font-bold uppercase text-center mt-10 mb-3">
-              Welcome !! {artisans?.fullName}
-            </h1>
-            <p className="font-semibold text-center">Here is your dashboard</p>
-          </div>
+		return (
+			<div>
+				<div className="flex items-center w-full">
+					{/* Header */}
+					<div className="w-[85%] pl-[8rem]">
+						<img
+							src={artisans.avatar}
+							alt=""
+							className="h-25 w-25 rounded-full"
+						/>
+						<h1 className="text-5xl font-bold uppercase text-center mt-10 mb-3">
+							Welcome !! {artisans?.fullName}
+						</h1>
+						<p className="font-semibold text-center">
+							Here is your dashboard
+						</p>
+					</div>
 
-          {/* Add new product */}
-          <div className="bg-three hover:bg-two rounded-full flex justify-center">
-            <Link to={"/artisans/productlisting"}>
-              <button className="p-5 font-bold ">Add new product</button>
-            </Link>
-          </div>
-        </div>
+					{/* Add new product */}
+					<div className="bg-three hover:bg-two rounded-full flex justify-center">
+						<Link to={"/artisans/productlisting"}>
+							<button className="p-5 font-bold ">
+								Add new product
+							</button>
+						</Link>
+					</div>
+				</div>
 
-        {/* artisans matrices */}
-        {artisansmatrices && (
-          <div className="flex justify-center p-auto">
-            <div className="grid grid-cols-4 mx-[8rem] my-20 gap-20">
-              <div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
-                <h2 className="text-xl ">Total Products</h2>
-                <p className="text-xl">
-                   {artisansproducts.length}
-                </p>
-              </div>
-              <div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
-                <h2 className="text-xl ">Customer satisfaction</h2>
-                <p className="text-xl">
-                  {artisansmatrices.metrics.customerSatisfaction}
-                </p>
-              </div>
-              <div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
-                <h2 className="text-xl ">Dispute Rate</h2>
-                <p className="text-xl">
-                  {artisansmatrices.metrics.disputeRate}
-                </p>
-              </div>
-              <div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
-                <h2 className="text-xl ">Product Selling Rate</h2>
-                <p className="text-xl">
-                  {artisansmatrices.metrics.productSellingRate}
-                </p>
-              </div>
-              <div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
-                <h2 className="text-xl ">Refund Rate</h2>
-                <p className="text-xl">{artisansmatrices.metrics.refundRate}</p>
-              </div>
-              <div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
-                <h2 className="text-xl ">Return Rate</h2>
-                <p className="text-xl">{artisansmatrices.metrics.returnRate}</p>
-              </div>
-            </div>
-          </div>
-        )}
+				{/* artisans matrices */}
+				{artisansmatrices && (
+					<div className="flex justify-center p-auto">
+						<div className="grid grid-cols-4 mx-[8rem] my-20 gap-20">
+							<div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
+								<h2 className="text-xl ">Total Products</h2>
+								<p className="text-xl">
+									{artisansproducts.length}
+								</p>
+							</div>
+							<div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
+								<h2 className="text-xl ">
+									Customer satisfaction
+								</h2>
+								<p className="text-xl">
+									{
+										artisansmatrices.metrics
+											.customerSatisfaction
+									}
+								</p>
+							</div>
+							<div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
+								<h2 className="text-xl ">Dispute Rate</h2>
+								<p className="text-xl">
+									{artisansmatrices.metrics.disputeRate}
+								</p>
+							</div>
+							<div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
+								<h2 className="text-xl ">
+									Product Selling Rate
+								</h2>
+								<p className="text-xl">
+									{
+										artisansmatrices.metrics
+											.productSellingRate
+									}
+								</p>
+							</div>
+							<div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
+								<h2 className="text-xl ">Refund Rate</h2>
+								<p className="text-xl">
+									{artisansmatrices.metrics.refundRate}
+								</p>
+							</div>
+							<div className="h-auto shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)] rounded-3xl bg-five hover:bg-three w-auto flex flex-col p-12 gap-5 justify-center items-center text-center">
+								<h2 className="text-xl ">Return Rate</h2>
+								<p className="text-xl">
+									{artisansmatrices.metrics.returnRate}
+								</p>
+							</div>
+						</div>
+					</div>
+				)}
 
-        {/* Charts */}
-        <div className="flex mx-[10rem] justify-between items-center flex-col lg:flex-row">
-          {/* Bar Graph */}
-          <div className="pt-10">
-            <BarChart width={600} height={400} data={data2}>
-              <Bar dataKey="sales" fill="green" />
-              <CartesianGrid stroke="#ccc" />
-              <XAxis dataKey="name" />
-              <YAxis />
-            </BarChart>
-          </div>
+				{/* Charts */}
+				<div className="flex mx-[10rem] justify-between items-center flex-col lg:flex-row">
+					{/* Bar Graph */}
+					<div className="pt-10">
+						<BarChart width={600} height={400} data={data2}>
+							<Bar dataKey="sales" fill="green" />
+							<CartesianGrid stroke="#ccc" />
+							<XAxis dataKey="name" />
+							<YAxis />
+						</BarChart>
+					</div>
 
-          {/* Pie Chart*/}
-          <div>
-            <PieChart width={400} height={400}>
-              <Pie
-                activeIndex={activeIndex}
-                data={data}
-                dataKey="students"
-                outerRadius={200}
-                fill="green"
-                onMouseEnter={onPieEnter}
-                style={{
-                  cursor: "pointer",
-                  outline: "none",
-                }} // Ensure no outline on focus
-              >
-                {data.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                  />
-                ))}
-              </Pie>
-              <Tooltip />
-            </PieChart>
-          </div>
-        </div>
+					{/* Pie Chart*/}
+					<div>
+						<PieChart width={400} height={400}>
+							<Pie
+								activeIndex={activeIndex}
+								data={data}
+								dataKey="students"
+								outerRadius={200}
+								fill="green"
+								onMouseEnter={onPieEnter}
+								style={{
+									cursor: "pointer",
+									outline: "none",
+								}} // Ensure no outline on focus
+							>
+								{data.map((entry, index) => (
+									<Cell
+										key={`cell-${index}`}
+										fill={COLORS[index % COLORS.length]}
+									/>
+								))}
+							</Pie>
+							<Tooltip />
+						</PieChart>
+					</div>
+				</div>
 
-        {/* artisans products */}
-        <div className="mx-[10rem] text-start">
-          <div className="text-3xl text-center p-8">Your Products</div>
-          {artisansproducts.map((product) => (
-            <div className="flex justify-between items-center p-4 border rounded-lg m-2 flex-col lg:flex-row">
-              <img src={product.images} alt="" className="h-20 w-20 " />
-              <div className="flex flex-col justify-between">
-                <h2 className="text-2xl uppercase"><b>Title:</b> {product.title}</h2>
-                <p className="text-lg uppercase"><b>Category:</b> {product.category.name}</p>
-              </div>
-              <p className="text-xl"><b className="uppercase">Description:</b> {product.description}</p>
-              <div className="text-xl">
-                <p><b className="uppercase">Stock:</b> {product.stockQuantity}</p>
-                <p><b className="uppercase">Sales:</b> {product.sales}</p>
-                <p><b className="uppercase">Views:</b> {product.views}</p>
-              </div>
+				{/* artisans products */}
+				<div className="mx-[10rem] text-start">
+					<div className="text-3xl text-center p-8">
+						Your Products
+					</div>
+					{artisansproducts.map((product) => (
+						<div className="flex justify-between items-center p-4 border rounded-lg m-2 flex-col lg:flex-row">
+							<img
+								src={product.images}
+								alt=""
+								className="h-20 w-20 "
+							/>
+							<div className="flex flex-col justify-between">
+								<h2 className="text-2xl uppercase">
+									<b>Title:</b> {product.title}
+								</h2>
+								<p className="text-lg uppercase">
+									<b>Category:</b> {product.category.name}
+								</p>
+							</div>
+							<p className="text-xl">
+								<b className="uppercase">Description:</b>{" "}
+								{product.description}
+							</p>
+							<div className="text-xl">
+								<p>
+									<b className="uppercase">Stock:</b>{" "}
+									{product.stockQuantity}
+								</p>
+								<p>
+									<b className="uppercase">Sales:</b>{" "}
+									{product.sales}
+								</p>
+								<p>
+									<b className="uppercase">Views:</b>{" "}
+									{product.views}
+								</p>
+							</div>
 
-              <div className="text-start flex lg:block">
-                <p className="text-xl font-bold uppercase">Price:</p>
-                <div className="text-xl flex justify-center items-center">
-                  <IndianRupee /> {product.price}
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
+							<div className="text-start flex lg:block">
+								<p className="text-xl font-bold uppercase">
+									Price:
+								</p>
+								<div className="text-xl flex justify-center items-center">
+									<IndianRupee /> {product.price}
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Dashboard;
--- src/components/Artisans/otherartisans/OtherArtisans.jsx
+++ src/components/Artisans/otherartisans/OtherArtisans.jsx
@@ -9,7 +9,9 @@ import { useParams } from "react-router-dom";
 function OtherArtisans() {
 	let { artisanid } = useParams();
 	const backendurl = import.meta.env.VITE_URL;
-	const [artisans, setArtisans] = useState(null);
+	const [artisans, setArtisans] = useState({});
+	const [artisansmatrices, setArtisansMatrices] =
+		useState(null);
 
 	const getCurrentArtisans = useCallback(async () => {
 		try {
@@ -25,17 +27,39 @@ function OtherArtisans() {
 				}
 			);
 			console.log(
-				"res in getcurrent artisans in other artisans",
-				res.data.data.artisan
+				" getcurrent artisans in other artisans",
+				res.data.data
 			);
 			setArtisans(res.data.data.artisan);
 		} catch (error) {
 			console.log("Error", error);
 		}
 	}, []);
+
+	const getArtisansSellerMetrices =
+		useCallback(async () => {
+			try {
+				const res = await axios.get(
+					`${backendurl}/artisans/matrices/${artisanid}`,
+					{
+						withCredentials: true,
+						headers: {
+							Authorization: `Bearer ${localStorage.getItem(
+								"artisansaccessToken"
+							)}`,
+						},
+					}
+				);
+				console.log("current artisans metrices", res.data);
+				setArtisansMatrices(res.data.data.metrics);
+			} catch (error) {
+				console.error("error in dashboard ", error);
+			}
+		}, [artisans?._id]);
 	useEffect(() => {
 		getCurrentArtisans();
-	}, [getCurrentArtisans]);
+		getArtisansSellerMetrices();
+	}, []);
 	return (
 		<div className="text-black">
 			<h1>Other Artisans</h1>
@@ -45,6 +69,14 @@ function OtherArtisans() {
 			<p>{artisans?.experience}</p>
 			<p>{artisans?.GSTIN}</p>
 			<img src={artisans?.avatar} alt="" />
+
+			<div>
+				<p>{artisansmatrices?.customerSatisfaction}</p>
+				<p>{artisansmatrices?.disputeRate}</p>
+				<p>{artisansmatrices?.productSellingRate}</p>
+				<p>{artisansmatrices?.refundRate}</p>
+				<p>{artisansmatrices?.returnRate}</p>
+			</div>
 		</div>
 	);
 }
