75d795bbfa16fcb129571251c037923e402f4e59
RabdeepKaur <ravleen0108@gmail.com>
2024-11-29 14:49:07 +0530

    small modification

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				one: "#002244", // background
+				one: "#0C084C", // background
 				two: "#F0EEC8", // boader and headline
 				three: "#00B7A8", // cream color on  button and such
 				four: "#F0EEC8", // fully background
