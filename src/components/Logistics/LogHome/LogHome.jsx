cab7ea1d4bad215151359fe2b57fedc8211f89f9
parv nema <115030244+PARVNEMA@users.noreply.github.com>
2024-12-04 12:50:02 +0530

    loghome

--- src/components/Logistics/LogHome/LogHome.jsx
+++ src/components/Logistics/LogHome/LogHome.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import axios from "axios";
+import React, {
+	useCallback,
+	useEffect,
+	useState,
+} from "react";
 
 function LogHome() {
-  return (
-    <div>
-      
-    </div>
-  )
+	const backendurl = import.meta.env.VITE_URL;
+	const [orders, setorders] = useState([]);
+
+	const getLogisticsOrders = useCallback(async () => {
+		try {
+			const res = await axios.get(
+				`${backendurl}/logistics/products`,
+				{
+					withCredentials: true,
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem(
+							"artisansaccessToken"
+						)}`,
+					},
+				}
+			);
+			console.log("logisctics orders=", res.data.data);
+			setorders(res.data.data);
+		} catch (error) {
+			console.error("error in logistics ", error);
+		}
+	}, []);
+
+	useEffect(() => {
+		getLogisticsOrders();
+	}, []);
+	return <div></div>;
 }
 
 export default LogHome;
